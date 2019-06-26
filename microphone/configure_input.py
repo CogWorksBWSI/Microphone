c56553a800b001d659b57961b3b7bf2dab52f9ba
Ryan Soklaski <ry26099@mit.edu>
2019-06-26 15:53:36 -0400

    tidy up configure_input

--- microphone/configure_input.py
+++ microphone/configure_input.py
@@ -6,27 +6,28 @@ import configparser
 import os
 from pathlib import Path
 
+from typing import Union, List, Optional
+
 _path = Path(os.path.dirname(os.path.abspath( __file__ )))
 
 
-def load_ini():
-    """ Returns the saved device from config.ini or `None`
+def load_ini() -> Union[dict, None]:
+    """Returns the saved device from config.ini or `None`
 
-            Returns
-            -------
-            Union[dict, None]
-                {name : device name,
-                 index: device index from config prompt}"""
+    Returns
+    -------
+    Union[dict, None]
+        {name : device name,
+         index: device index from config prompt}"""
     config = configparser.ConfigParser()
 
     # This returns an empty array if no config file was found
     if config.read(str(_path / 'config.ini')) != []:
         return config['input device']
-    else:
-        return None
+    return None
 
 
-def get_input_devices():
+def get_input_devices() -> List[dict]:
     """ Use pyaudio to detect available mic-devices.
 
         Returns
@@ -36,31 +37,32 @@ def get_input_devices():
             the device."""
     p = pyaudio.PyAudio()
     devices = [p.get_device_info_by_index(index) for index in range(0, p.get_device_count())]
-    inputDevices = [device for device in devices if device['maxInputChannels'] > 0]
-    return inputDevices
+    return [device for device in devices if device['maxInputChannels'] > 0]
 
 
-def present_menu(devices, savedDevice=None):
+def present_menu(devices: List[dict], daved_device: Optional[dict] = None) -> dict:
     """ Format the menu of selectable microphones and receive selection from user.
 
-        Parameters
-        ----------
-        devices : List[dict]
-            Device logs for detected microphones
+    Parameters
+    ----------
+    devices : List[dict]
+        Device logs for detected microphones
 
-        savedDevice : Optional[dict]
-             The device log for the saved device - used to indicate on the menu the current
-             saved device.
+    daved_device : Optional[dict]
+         The device log for the saved device - used to indicate on
+         the menu the current saved device.
 
-        Returns
-        -------
-        dict
-            The device log for the user-selected device."""
+    Returns
+    -------
+    dict
+        The device log for the user-selected device."""
     # Print the menu
     menuIndex = 0
     for device in devices:
         # If the user previously saved a device, indicate that in the menu with an asterisk
-        isSavedSelection = int(savedDevice['index']) == device['index'] if savedDevice is not None else False
+        isSavedSelection = int(daved_device['index']) == device['index'] \
+            if daved_device is not None \
+            else False
         savedSelectionText = '*' if isSavedSelection else ' '
         
         print('{}) {} {}'.format(str(menuIndex), savedSelectionText, device['name']))
@@ -82,18 +84,18 @@ def present_menu(devices, savedDevice=None):
     return devices[menuSelection]
 
 
-def save_ini(selectedDevice):
+def save_ini(selected_device: dict):
     """ Saves device to songfp/mic_config/config.ini
 
         Parameters
         ----------
-        selectedDevice : dict
+        selected_device : dict
             {name : device name,
              index: device index from config prompt}"""
     config = configparser.ConfigParser()
     config['input device'] = {
-        'name': selectedDevice['name'],
-        'index': selectedDevice['index']
+        'name': selected_device['name'],
+        'index': selected_device['index']
     }
     with open(os.path.join(_path, 'config.ini'), 'w') as configfile:
         config.write(configfile)
